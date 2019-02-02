2b7a60075ed34ef855e09e110ac9c5197e419fdb
avadhutthorat <avadhutthorat40@gmail.com>
2019-02-02 23:55:27 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 import Index from "./components/layout/Index";
 import Lyrics from "./components/tracks/Lyrics";
 import { Provider } from "./Context";
@@ -18,6 +19,7 @@ class App extends Component {
               <Route exact path="/lyrics/track/:id" component={Lyrics} />
             </Switch>
             <div className="container" />
+            <Footer />
           </div>
         </Router>
       </Provider>
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <React.Fragment>
+      <footer class="page-footer font-small special-color-dark pt-4">
+        <div class="container" />
+
+        <div class="footer-copyright text-center py-3">
+          © 2019 Copyright <br /> Made in <i class="fa fa-heart text-danger" />{" "}
+          with ReactJs
+        </div>
+      </footer>
+    </React.Fragment>
+  );
+};
+
+export default Footer;
