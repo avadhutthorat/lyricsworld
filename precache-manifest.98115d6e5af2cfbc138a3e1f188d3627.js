84cd594c9367af280d2abd150fa6c903b9172c48
avadhutthorat <avadhutthorat40@gmail.com>
2019-02-03 12:02:54 +0530

    Updates

---
+++ precache-manifest.98115d6e5af2cfbc138a3e1f188d3627.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d4ff95f018bfd48c037",
+    "url": "/lyricsworld/static/css/main.6f767a65.chunk.css"
+  },
+  {
+    "revision": "3d4ff95f018bfd48c037",
+    "url": "/lyricsworld/static/js/main.3d4ff95f.chunk.js"
+  },
+  {
+    "revision": "0f1e936ce231f2d781ef",
+    "url": "/lyricsworld/static/js/1.0f1e936c.chunk.js"
+  },
+  {
+    "revision": "28d22ac162902dfa6a93",
+    "url": "/lyricsworld/static/js/runtime~main.28d22ac1.js"
+  },
+  {
+    "revision": "a31b56cf71773b53e022d1535566f57a",
+    "url": "/lyricsworld/index.html"
+  }
+];
